147e476b32a1f994b45e804df53f9976c50f2f07
haenanim121@gmail.com <haenanim121@gmail.com>
2023-11-22 15:22:10 +0900

    fix : 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import Navbar from '@/components/Navbar';
