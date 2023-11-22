689d9b9da8a7bd4681787adf17e694dda3132a7a
haenanim121@gmail.com <haenanim121@gmail.com>
2023-11-22 14:55:38 +0900

    fix : 로고추가 및 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,16 @@
-// 'use client';
+'use client';
 import React from 'react';
 import { MdMoreVert } from 'react-icons/md';
+import Logo from '@/image/Logo.png';
+import Image from 'next/image';
 
 function Header() {
   return (
     <div className="flex h-[60px] bg-white">
-      <div className="w-[60px] bg-gray-400 flex items-center justify-center">
-        <div className="">Icon</div>
+      <div className="w-[60px] bg-[#E00685] flex items-center justify-center">
+        <div className="">
+          <Image src={Logo} alt="v13 image" width={500} height={100} />
+        </div>
       </div>
       <div className="flex-1  flex items-center m-[10px] text-[24px] font-[800]">
         헤더내용
