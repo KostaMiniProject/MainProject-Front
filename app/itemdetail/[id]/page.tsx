1741e77f5b4f1e42d6d3ef36ff4b5303e9c9b6f7
haenanim121@gmail.com <haenanim121@gmail.com>
2023-11-27 19:17:49 +0900

    feat : 묽건 상세 페이지 구성

--- app/itemdetail/[id]/page.tsx
+++ app/itemdetail/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getItemDetailById } from '@/api/ItemApi';
+import Button from '@/components/Button';
 import Header from '@/components/Header';
 import Profile from '@/components/Profile';
 import Carousel from '@/components/carousel/Carousel';
@@ -7,13 +8,6 @@ import React from 'react';
 
 function Page() {
   const detail = getItemDetailById(1);
-
-  if (!detail) {
-    // detail이 유효하지 않은 경우에 대한 처리
-    return <div>Error loading item details</div>;
-  }
-
-  // detail.image_url이 배열 형태로 제공되지 않는다면 수정 필요
   const images =
     'https://kosta-main-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20231127_174618039.png';
 
@@ -24,7 +18,17 @@ function Page() {
         <Carousel images={detail.image_url} />
       </div>
       <Profile profile={detail.profile} />
-      <Image key={1} src={images} width={60} height={60} alt={`Image`} />
+      <div className="mx-[15px]">
+        <div className="py-[5px] border-solid border-b-[0.5px] ">
+          <div className="text-[25px] font-[600]">{detail.title}</div>
+          <div className="flex">
+            <Button text={detail.category} btnStyle="tag" height={5} />
+          </div>
+        </div>
+        <div className="min-h-[300px] my-[5px] bg-softbase rounded-[5px] ">
+          <div className="py-[5px] font-[600]">{detail.description}</div>
+        </div>
+      </div>
     </div>
   );
 }
