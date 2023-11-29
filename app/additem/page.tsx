486a2d8b96499dc462ce59dd747306525fea4f4e
haenanim121@gmail.com <haenanim121@gmail.com>
2023-11-29 16:26:22 +0900

    fix : 컴포넌트 구조 변경

---
+++ app/additem/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <Header backNav title="아이템 추가"></Header>
+      <div></div>
+    </div>
+  );
+}
+
+export default page;
--- app/myitem/page.tsx
+++ app/myitem/page.tsx
@@ -1,25 +1,34 @@
-"use client";
-import { getProfile } from "@/api/ProfileApi";
-import Header from "@/components/Header";
-import Profile from "@/components/Profile";
-import React from "react";
+'use client';
+import Header from '@/components/Header';
+import React, { useState } from 'react';
 
-import Item from "@/components/item/Item";
-import BottomFixed from "@/components/BottomFixed";
-import Button from "@/components/Button";
-import { useRouter } from "next/navigation";
-import Scarch from "@/components/Search";
-import { getItemList } from "@/api/ItemApi";
+import Item from '@/components/item/Item';
+import BottomFixed from '@/components/BottomFixed';
+import Button from '@/components/Button';
+import { useRouter } from 'next/navigation';
+import { getItemList } from '@/api/ItemApi';
+import InputBox from '@/components/InputBox';
+import { MdOutlineSearch } from 'react-icons/md';
 
 function page() {
   const itemList = getItemList();
   const route = useRouter();
+  const [keyword, setKeyWord] = useState<String>('');
 
   return (
     <div className="relative">
       <Header title="내 물건" backNav></Header>
       {/* 검색창 */}
-      <Scarch />
+      <div className=" h-[60px] flex items-center border-b-[1px] border-gray">
+        <InputBox onChange={setKeyWord} />
+        <div
+          onClick={() => {
+            console.log(keyword);
+          }}
+        >
+          <MdOutlineSearch size={40} />
+        </div>
+      </div>
       {/* 물건 리스트 */}
       <div className="mx-[15px]">
         {itemList.map((e: any, i: any) => {
