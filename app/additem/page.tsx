6f8fcf824f548239b155abba57fa72dfe238faff
haenanim121@gmail.com <haenanim121@gmail.com>
2023-11-30 10:02:44 +0900

    feat : 사진 업로드 섹션 개발

--- app/additem/page.tsx
+++ app/additem/page.tsx
@@ -5,6 +5,7 @@ import InputBox from '@/components/InputBox';
 import TextAreaBox from '@/components/TextAreaBox';
 import Image from 'next/image';
 import React, { useState } from 'react';
+import { MdAddCircle, MdAddCircleOutline, MdCancel } from 'react-icons/md';
 
 function Page() {
   const [title, setTitle] = useState<String>('');
@@ -14,10 +15,25 @@ function Page() {
   function handleMoreView() {
     setIsMoreView(!isMoreView);
   }
+  function removeImage(index: number) {
+    // 이미지 삭제
+    setSelectedImages((prevImages) => {
+      const newImages = [...prevImages];
+      newImages.splice(index, 1);
+      return newImages;
+    });
+  }
 
-  function handleFileChange(event: any) {
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const files = event.target.files;
-    setSelectedImages(Array.from(files));
+    if (!files || files.length === 0) return;
+
+    // 이전 이미지 배열에 새로운 이미지 배열을 추가
+    setSelectedImages((prevImages) => {
+      // 현재 이미지 배열과 새로운 이미지 배열의 합이 5를 초과하면 슬라이스하여 최대 5개로 제한
+      const newImages = [...prevImages, ...Array.from(files)];
+      return newImages.length > 5 ? newImages.slice(0, 5) : newImages;
+    });
   }
 
   function postComplete() {
@@ -51,18 +67,28 @@ function Page() {
                 layout="fill"
                 objectFit="cover"
               />
+              <div
+                className="absolute top-0 right-0 cursor-pointer"
+                onClick={() => removeImage(index)}
+              >
+                <MdCancel size={30} color={'#e00685'} />
+              </div>
             </div>
           ))}
-          <div
-            className="relative w-[120px] h-[120px] overflow-hidden m-[5px] flex items-center justify-center border-base border-solid border-[1px] rounded-[10px]"
-            onClick={openFileInput}
-          >
-            <div>+</div>
-          </div>
+          {selectedImages.length < 5 ? (
+            <div
+              className="relative w-[120px] h-[120px] overflow-hidden m-[5px] flex items-center justify-center border-base border-solid border-[1px] rounded-[10px]"
+              onClick={openFileInput}
+            >
+              <MdAddCircleOutline size={30} color={'#e00685'} />
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
         <div
           onClick={handleMoreView}
-          className="bg-base h-[120px] w-[80px] flex items-center justify-center cursor-pointer"
+          className="bg-base rounded-[5px] m-[10px] h-[120px] w-[60px] flex items-center justify-center cursor-pointer"
         >
           <div className="text-white">{isMoreView ? '▲' : '▼'}</div>
         </div>
