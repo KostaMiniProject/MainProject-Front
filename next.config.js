bbbb2cc3835a726e8e34dbf93ea618bb18c1a167
haenanim121@gmail.com <haenanim121@gmail.com>
2023-12-05 16:48:47 +0900

    feat : Item add api 생성

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination: 'https://itsop.shop/:path*',
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
