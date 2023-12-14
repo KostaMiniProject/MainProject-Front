d89b2093178fe38230b132adfb9e62d06c3b21f2
haenanim121@gmail.com <haenanim121@gmail.com>
2023-12-14 16:44:46 +0900

    fix : 오타수정

--- api/ExchangePostApi.tsx
+++ api/ExchangePostApi.tsx
@@ -66,3 +66,19 @@ export async function updateExchangePost(jsonData: any, postId: number) {
     console.error('Error fetching data:', error);
   }
 }
+export async function deleteExchangePost(postId: number) {
+  try {
+    const result = await commonFetch(
+      `https://itsop.shop/api/exchange-posts/${postId}`,
+      {
+        method: 'DELETE',
+        checkToken: true, // 이 옵션이 있는 경우에만 토큰이 추가됨
+        // 기타 다른 옵션들...
+      }
+    );
+
+    console.log('Fetched data:', result);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
