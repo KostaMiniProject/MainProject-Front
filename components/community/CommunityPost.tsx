9cab3c44d505176a775b129c656ca82aa4c5a4f1
haenanim121@gmail.com <haenanim121@gmail.com>
2023-12-14 16:59:11 +0900

    fix : 커뮤니티 포스트 컴포넌트 수정

--- components/community/CommunityPost.tsx
+++ components/community/CommunityPost.tsx
@@ -3,32 +3,29 @@ import Carousel from '../carousel/Carousel';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { MdBookmarkBorder, MdBookmark } from 'react-icons/md';
 import { MdChatBubbleOutline } from 'react-icons/md';
-import { IoIosSend } from "react-icons/io";
-import { FaAngleRight } from "react-icons/fa";
+import { IoIosSend } from 'react-icons/io';
+import { FaAngleRight } from 'react-icons/fa';
 
-const communityTag = [
-  '#식품',
-  '#생활용품',
-  '#카테고리'
-]
+const communityTag = ['#식품', '#생활용품', '#카테고리'];
+const images = ['', ''];
 
-function CommunityPost() {
+function CommunityPost({ postItem }: { postItem?: any }) {
   return (
     <div className="w-full p-[10px] flex border-solid border-[0.5px] border-gray rounded-[5px] my-[5px]">
       <div></div>
       <div className="w-full ">
         <div className="flex mb-[10px]">
           <div className="w-[40px] h-[40px] bg-black rounded-[50%]"></div>
-          <div className='flex justify-center items-center text-gray ml-[10px]'>
+          <div className="flex justify-center items-center text-gray ml-[10px]">
             닉네임
             <FaAngleRight size={15} />
           </div>
         </div>
         <div className="w-[100%] h-[auto]">
           {/* 캐러셀에 대한 설정 */}
-          <Carousel images={[{}, {}]}></Carousel>
+          {postItem && <Carousel images={postItem}></Carousel>}
         </div>
-        <div className='flex justify-between my-[5px]'>
+        <div className="flex justify-between my-[5px]">
           <div className="flex">
             <FaRegHeart size={25} />
             <FaHeart size={25} />
@@ -41,9 +38,7 @@ function CommunityPost() {
           </div>
         </div>
         <div className="flex text-[20px] font-[600] mt-[5px]">제목</div>
-        <div className="text-gray mr-[10px] text-subtitle">
-          날짜
-        </div>
+        <div className="text-gray mr-[10px] text-subtitle">날짜</div>
         <div className="whitespace-nowrap text-ellipsis overflow-hidden">
           물건명
         </div>
