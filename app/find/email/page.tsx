0e051e071fa123ef7a180e785d15ed3829f64dd1
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-19 12:45:00 +0900

    [Feat] Header, placeholder 추가 및 margin 값 변경

--- app/find/email/page.tsx
+++ app/find/email/page.tsx
@@ -4,6 +4,7 @@ import InputBox from "@/components/InputBox";
 import Button from "@/components/Button";
 import { useRouter } from "next/navigation";
 import { findId } from "@/api/FindIdApi";
+import Header from "@/components/Header";
 
 function Page() {
   const [name, setName] = useState<string>("");
@@ -15,35 +16,45 @@ function Page() {
       // 유효성 검사를 통과한 경우 회원가입 요청
       const userData = {
         name: name,
-        phone: phone
+        phone: phone,
       };
-        const findEmail = await findId(userData);
-        // console.log('Email:', findEmail.email);
-        // Do something with the email data
-      
+      const findEmail = await findId(userData);
+      // console.log('Email:', findEmail.email);
+      // Do something with the email data
+
       // findId 함수에서 서버에서 에러가 발생하지 않는다면, '/success'로 이동
       router.push(`/find/email/success?email=${findEmail.email}`);
     } catch (error: any) {
-        // 'error' 변수를 'any' 타입으로 선언하여 사용
-        alert((error as Error).message || '입력한 정보가 일치하지 않습니다.');
-      }
+      // 'error' 변수를 'any' 타입으로 선언하여 사용
+      alert((error as Error).message || "입력한 정보가 일치하지 않습니다.");
+    }
   }
 
   return (
     <div>
+      <Header title="아이디 찾기"></Header>
       <div className="w-full flex-col flex">
-        <div className="text-[40px] mt-[20px] mb-[150px]">ID찾기</div>
+        <div className="text-[20px] mt-[150px] mb-[50px] m-[auto] font-[600]">
+          아래내용을 작성해주세요.
+        </div>
       </div>
       <div className="mx-[15px]">
         <div className="">
-          <div className="text-[20px] my-[10px] font-[600]">이름</div>
-          <InputBox onChange={setName}></InputBox>
+          <div className="text-[20px]">이름</div>
+          <InputBox
+            onChange={setName}
+            message="이름을 입력해주세요."
+          ></InputBox>
         </div>
         <div className="my-[20px]">
-          <div className="text-[20px] pt-[50px] my-[10px] font-[600]">휴대전화번호</div>
-          <InputBox onChange={setPhone} type="password"></InputBox>
+          <div className="text-[20px] pt-[10px]">전화번호</div>
+          <InputBox
+            onChange={setPhone}
+            type="password"
+            message="전화번호를 입력해주세요. (ex) 010-1234-1234"
+          ></InputBox>
         </div>
-        <div className="text-center pt-[50px] my-[20px]">
+        <div className="text-center pt-[50px] my-[10px]">
           <Button
             text="아이디 찾기"
             fontSize={18}
--- app/find/password/page.tsx
+++ app/find/password/page.tsx
@@ -4,6 +4,7 @@ import InputBox from "@/components/InputBox";
 import Button from "@/components/Button";
 import { useRouter } from "next/navigation";
 import { findPassword } from "@/api/FindPasswordApi";
+import Header from "@/components/Header";
 
 function Page() {
   const [email, setEmail] = useState<string>("");
@@ -12,52 +13,68 @@ function Page() {
 
   const router = useRouter();
 
-  async function handleFindPassword(email: string, name: string, phone: string) {
+  async function handleFindPassword(
+    email: string,
+    name: string,
+    phone: string
+  ) {
     try {
       // 유효성 검사를 통과한 경우 회원가입 요청
       const userData = {
         email: email,
         name: name,
-        phone: phone
+        phone: phone,
       };
-        const findEmail = await findPassword(userData);
-        // console.log('Email:', findEmail.email);
-        // Do something with the email data
-      
+      const findEmail = await findPassword(userData);
+      // console.log('Email:', findEmail.email);
+      // Do something with the email data
+
       // findId 함수에서 서버에서 에러가 발생하지 않는다면, '/success'로 이동
-      router.push('/find/password/success');
+      router.push("/find/password/success");
     } catch (error: any) {
-        // 'error' 변수를 'any' 타입으로 선언하여 사용
-        alert((error as Error).message || '입력한 정보가 일치하지 않습니다.');
-      }
+      // 'error' 변수를 'any' 타입으로 선언하여 사용
+      alert((error as Error).message || "입력한 정보가 일치하지 않습니다.");
+    }
   }
 
   return (
     <div>
+      <Header title="비밀번호 찾기"></Header>
       <div className="w-full flex-col flex">
-        <div className="text-[40px] mt-[20px] mb-[60px]">비밀번호 찾기</div>
+        <div className="text-[20px] mt-[150px] mb-[50px] m-[auto] font-[600]">
+          아래내용을 작성해주세요.
+        </div>
       </div>
       <div className="mx-[15px]">
         <div className="">
-          <div className="text-[20px] my-[10px] font-[600]">이메일</div>
-          <InputBox onChange={setEmail}></InputBox>
+          <div className="text-[20px] my-[10px]">이메일</div>
+          <InputBox
+            onChange={setEmail}
+            message="이메일을 입력해주세요."
+          ></InputBox>
         </div>
         <div className="">
-          <div className="text-[20px] mt-[50px] my-[10px] font-[600]">이름</div>
-          <InputBox onChange={setName}></InputBox>
+          <div className="text-[20px] mt-[10px] my-[10px]">이름</div>
+          <InputBox
+            onChange={setName}
+            message="이름을 입력해주세요."
+          ></InputBox>
         </div>
         <div className="">
-          <div className="text-[20px] mt-[50px] my-[10px] font-[600]">전화번호</div>
-          <InputBox onChange={setPhone}></InputBox>
+          <div className="text-[20px] mt-[10px] my-[10px]">전화번호</div>
+          <InputBox
+            onChange={setPhone}
+            message="전화번호를 입력해주세요. (ex) 010-1234-1234"
+          ></InputBox>
         </div>
-        <div className="text-center pt-[50px] my-[20px]">
+        <div className="text-center pt-[50px] my-[10px]">
           <Button
             text="비밀번호 찾기"
             fontSize={18}
             height={5}
             rounded="soft"
             onClick={() => {
-              handleFindPassword(email,name, phone);
+              handleFindPassword(email, name, phone);
             }}
           ></Button>
         </div>
