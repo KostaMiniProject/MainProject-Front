b4ca160035bcd2b07dd253921c1699a482110171
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-20 15:46:20 +0900

    아이디 찾기 결과 페이지 수정

--- app/find/email/success/page.tsx
+++ app/find/email/success/page.tsx
@@ -1,17 +1,36 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import Header from "@/components/Header";
+import Button from "@/components/Button";
 
 function Success() {
   const router = useRouter();
 
   const searchParams = useSearchParams();
-  
 
   return (
     <div className="container mx-auto mt-10">
-      <h1 className="text-[40px] mt-[20px] mb-[150px]">이메일 확인</h1>
-      <p className="text-lg">고객님의 이메일은 다음과 같습니다 <span className="font-bold">{searchParams.get('email')}</span></p>
+      <Header title="아이디 찾기"></Header>
+      <div className="w-full flex-col flex">
+        <div className="text-[12px] mt-[100px] mb-[10px] m-[auto] ">
+          고객님 아이디 찾기가 완료 되었습니다 :)
+        </div>
+      </div>
+      <div className="w-full flex-col flex border border-gray p-5 rounded-md mb-[10px]">
+        <div className="text-[16px] m-[auto] font-[600]">
+          Email : {searchParams.get("email")}
+        </div>
+      </div>
+      <div className="w-full flex-col flex">
+        <Button
+          text="작성 완료"
+          fontSize={10}
+          height={8}
+          width={10}
+          rounded="soft"
+        />
+      </div>
     </div>
   );
 }
