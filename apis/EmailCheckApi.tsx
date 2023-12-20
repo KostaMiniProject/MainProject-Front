e6ef55b4d43161062a5b7802745d5e270b162aae
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-20 17:54:30 +0900

    이메일 인증 API 추가

---
+++ apis/EmailCheckApi.tsx
@@ -0,0 +1,26 @@
+"use client";
+export async function postEmailCheck(userData: any) {
+  try {
+    const res = await fetch("https://itsop.shop/api/authNumCheck", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!res.ok) {
+      // 로그인 실패 처리
+      throw new Error(`이메일 인증 실패: ${res.status}`);
+    }
+
+    // const data = await res.json();
+    // 토큰을 반환하거나 저장, 사용 등을 수행
+
+    // return [data];
+  } catch (error) {
+    // 오류 처리
+    console.error("이메일 인증 오류 발생:", error);
+    throw error; // 호출자에게 오류 전파
+  }
+}
