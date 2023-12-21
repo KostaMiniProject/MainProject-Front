d3fb2b362c07ccde75b9524c1acce2264cd33022
SCM <hys1693359@gmail.com>
2023-12-21 10:15:57 +0900

    채팅방 나가기 테스트

--- apis/ChattingApi.tsx
+++ apis/ChattingApi.tsx
@@ -3,7 +3,9 @@ import { commonFetch } from './commonApi/CommonFetch';
 
 export async function getChatRoomList() {
   try {
-    const result = await commonFetch('https://itsop.shop/api/chatRooms', {
+    // http://localhost:8080
+    // https://itsop.shop
+    const result = await commonFetch('http://localhost:8080/api/chatRooms', { 
       method: 'GET',
       checkToken: true, // 이 옵션이 있는 경우에만 토큰이 추가됨
       // 기타 다른 옵션들...
@@ -18,7 +20,7 @@ export async function getChatRoomList() {
 
 export async function postCreateRoom(body: any) {
   try {
-    const result = await commonFetch('https://itsop.shop/api/chatRooms', {
+    const result = await commonFetch('http://localhost:8080/api/chatRooms', {
       method: 'POST',
       checkToken: true, // 이 옵션이 있는 경우에만 토큰이 추가됨
       // 기타 다른 옵션들...
